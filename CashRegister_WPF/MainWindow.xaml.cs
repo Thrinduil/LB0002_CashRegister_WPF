1125f8fdd538218e663b6dafb30f91081d96fe30
Fredrik Söderquist <f.soderquist@gmail.com>
2020-02-23 15:51:25 +0100

    Add logic

--- CashRegister_WPF/MainWindow.xaml.cs
+++ CashRegister_WPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace CashRegister_WPF
 {
@@ -23,6 +14,80 @@ namespace CashRegister_WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public Tuple<int, int> GetPriceAndPayed()
+        {
+            int price = 0;
+            int payed = 0;
+
+            if (Int32.TryParse(tbxPrice.Text, out price) && Int32.TryParse(tbxPayed.Text, out payed))
+            {
+                if (price > payed)
+                {
+                    MessageBox.Show("Kunden betalade för lite. Snåljåp.", "Fel");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kunde inte kovertera Pris eller Betalat till heltal.", "Fel");
+            }
+
+            return new Tuple<int, int>(price, payed);
+        }
+
+        private void BtnCalcChange_Click(object sender, RoutedEventArgs e)
+        {
+            CashRegister cashRegister = new CashRegister();
+
+            Tuple<int, int> priceAndPayed = GetPriceAndPayed();
+
+            List<int> change = cashRegister.MakeChange(priceAndPayed.Item1, priceAndPayed.Item2);
+
+            DisplayChange(change);
+        }
+
+        private void DisplayChange(List<int> change)
+        {
+            lbl1000.Content = change.Where(item => item == 1000).Count();
+            lbl500.Content = change.Where(item => item == 500).Count();
+            lbl200.Content = change.Where(item => item == 200).Count();
+            lbl100.Content = change.Where(item => item == 100).Count();
+            lbl50.Content = change.Where(item => item == 50).Count();
+            lbl20.Content = change.Where(item => item == 20).Count();
+            lbl10.Content = change.Where(item => item == 10).Count();
+            lbl5.Content = change.Where(item => item == 5).Count();
+            lbl2.Content = change.Where(item => item == 2).Count();
+            lbl1.Content = change.Where(item => item == 1).Count();
+        }
+    }
+
+    class CashRegister
+    {
+        private readonly int[] drawer = new int[] { 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 }
+
+        public List<int> MakeChange(int price, int payed)
+        {
+            int difference = payed - price;
+
+            List<int> change = new List<int> { };
+            int i = 0;
+            int denomination = drawer[i];
+
+            while (difference > 0)
+            {
+                if (difference < denomination)
+                {
+                    i += 1;
+                    denomination = drawer[i];
+                    continue;
+                }
+
+                change.Add(denomination);
+                difference -= denomination;
+            }
+
+            return change;
         }
     }
 }
